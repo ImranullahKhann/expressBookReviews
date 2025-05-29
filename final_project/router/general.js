ec4cd4a8c49775ef40a07b8477d189b4b1c656de
ImranullahKhann <imranullahkhann2004@gmail.com>
2025-05-29 15:35:11 +0500

    Finished.

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -23,24 +23,24 @@ public_users.post("/register", (req,res) => {
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
+public_users.get('/',async function (req, res) {
   //Write your code here
-    res.send(JSON.stringify(books,null,4))
+    setTimeout(res.send(JSON.stringify(books,null,4)), 500);
 });
 
 // Get book details based on ISBN
-public_users.get('/isbn/:isbn',function (req, res) {
+public_users.get('/isbn/:isbn',async function (req, res) {
   //Write your code here
     const isbn = req.params.isbn;
-    res.send(JSON.stringify(books[isbn], null, 4))
+    setTimeout(res.send(JSON.stringify(books[isbn], null, 4)), 500)
  });
   
 // Get book details based on author
-public_users.get('/author/:author',function (req, res) {
+public_users.get('/author/:author', async function (req, res) {
   //Write your code here
   const author = req.params.author
   const booksByAuthor = Object.values(books).filter(book => book.author.toLowerCase() === author.toLowerCase().replace("-", " "))
-  res.send(JSON.stringify(booksByAuthor[0], null, 4))
+  setTimeout(res.send(JSON.stringify(booksByAuthor[0], null, 4)), 500)
 });
 
 // Get all books based on title
@@ -48,7 +48,7 @@ public_users.get('/title/:title',function (req, res) {
   //Write your code here
   const title = req.params.title
   const bookByTitle = Object.values(books).filter(book => book.title.toLowerCase() === title.toLowerCase().replace("-", " "))
-  res.send(JSON.stringify(bookByTitle[0], null, 4))
+  setTimeout(res.send(JSON.stringify(bookByTitle[0], null, 4)), 500)
 });
 
 //  Get book review
